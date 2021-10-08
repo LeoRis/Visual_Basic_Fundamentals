1e727440f4dbde478ee641c61c08d9c43aa33238
Leo Ris <ristanovski@hotmail.com>
2021-10-08 21:46:17 +0200

    Updating the VB demo.

--- VBFundamentals/MainModule.vb
+++ VBFundamentals/MainModule.vb
@@ -40,7 +40,8 @@
 
         Dim Subject As String = "Die Hard With A Vengance."
 
-        Console.WriteLine("Built-in String Methods")
+        'vbCrLf is used to add a new row.
+        Console.WriteLine(vbCrLf & "Built-in String Methods")
         Console.WriteLine(Subject.Length)
         Console.WriteLine(Subject.IndexOf(" "))
         Console.WriteLine(Subject.EndsWith("e"))
@@ -50,8 +51,49 @@
         Console.WriteLine(Subject.ToUpper())
         Console.WriteLine(Subject.ToLower())
 
-        Console.ReadKey()
+        Dim Price As Decimal = 999.99D
 
+        'vbCrLf is used to add a new row.
+        Console.WriteLine(vbCrLf & "Built-in Numeric Methods")
+        Console.WriteLine(Price.Equals(999.99D))
+        Console.WriteLine(Decimal.MinValue)
+        Console.WriteLine(Decimal.MaxValue)
+        Console.WriteLine(Decimal.Ceiling(Price)) 'The next highest whole number
+        Console.WriteLine(Decimal.Floor(Price)) 'The next lowest whole number
+
+        Decimal.TryParse("55.01", Price)
+        Console.WriteLine(Price)
+
+        Dim SellDate As DateTime = #9/13/1990 08:00:01 AM#
+
+        'vbCrLf is used to add a new row.
+        Console.WriteLine(vbCrLf & "Build-in DateTime Methods")
+        Console.WriteLine(SellDate.AddDays(10))
+        Console.WriteLine(SellDate.AddDays(-10))
+        Console.WriteLine(SellDate.AddYears(31))
+        Console.WriteLine(SellDate.AddYears(-2))
+
+        Console.WriteLine(SellDate.Date)
+        Console.WriteLine(SellDate.Day)
+        Console.WriteLine(SellDate.DayOfWeek)
+        Console.WriteLine(SellDate.Year)
+        Console.WriteLine(SellDate.DayOfYear)
+
+        Console.WriteLine(SellDate.Hour)
+        Console.WriteLine(SellDate.Minute)
+        Console.WriteLine(SellDate.Second)
+
+        Dim prod As New Product
+        Dim sellingDate As DateTime
+
+        prod.SellStartDate = #1/1/2021#
+        prod.CalculateSellEndDate(20, sellingDate)
+
+        Console.WriteLine(sellingDate)
+
+        Console.WriteLine("This is the end date " + prod.SellEndDate)
+
+        Console.ReadKey()
     End Sub
 
     Sub IncrementListPrice()
---
+++ VBFundamentals/Product.vb
@@ -0,0 +1,58 @@
+﻿Public Class Product
+    Private _IsActive As Boolean
+    Public Property IsActive() As Boolean
+        Get
+            Return _IsActive
+        End Get
+        Set(ByVal value As Boolean)
+            _IsActive = value
+        End Set
+    End Property
+
+    Private _Name As String
+    Public Property Name() As String
+        Get
+            Return _Name
+        End Get
+        Set(ByVal value As String)
+            _Name = value
+        End Set
+    End Property
+
+    Private _ProductNumber As String
+    Public Property ProductNumber() As String
+        Get
+            Return _ProductNumber
+        End Get
+        Set(ByVal value As String)
+            _ProductNumber = value
+        End Set
+    End Property
+
+    'Auto-implemented properties.
+    'Public Property IsActive As Boolean
+    'Public Property Name As String
+    'Public Property ProductNumber As String
+
+    Public ReadOnly Property NameAndNumber() As String
+        Get
+            Return Name + "-" + ProductNumber
+        End Get
+    End Property
+
+    Public Property Color As String
+    Public Property StandardCost As Decimal
+    Public Property ListPrice As Decimal
+    Public Property Size As String
+    Public Property Weight As Decimal
+    Public Property SellStartDate As DateTime
+    Public Property SellEndDate As DateTime
+
+    Sub CalculateSellEndDate(ByVal days As Integer,
+                             ByRef sellDate As DateTime)
+        SellEndDate = SellStartDate.AddDays(days)
+        'Set the ByRef parameter
+        sellDate = SellEndDate
+    End Sub
+
+End Class
